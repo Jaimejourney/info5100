326b83604073075d68e6cd6a17296a884ed24038
Jaimejourney <jaimejourney@163.com>
2018-10-24 15:08:41 -0700

    Assignment 6

---
+++ Assignment6/src/Dog.java
@@ -0,0 +1,53 @@
+import java.util.Calendar;
+
+public class Dog extends Pet implements Boardable {
+    private String size;
+    private Calendar start = Calendar.getInstance();
+    private Calendar end = Calendar.getInstance();
+
+    public Dog(String petName, String ownerName, String color,String size) {
+        super(petName, ownerName, color);
+        this.size = size;
+    }
+
+    public String getSize(){
+        return this.size;
+    }
+
+    public String toString() {
+        String body = super.toString();
+        return "DOG:\n" + body + "\nSize: " + getSize();
+    }
+
+    @Override
+    public void setBoardStart(int month, int day, int year) {
+        start.set(month,day,year);
+    }
+
+    @Override
+    public void setBoardEnd(int month, int day, int year) {
+        end.set(month,day,year);
+    }
+
+    @Override
+    public boolean boarding(int month, int day, int year) {
+        Calendar cur = Calendar.getInstance();
+        cur.set(year, month, day);
+        return start.before(cur) && end.after(cur) || cur.equals(start) || cur.equals(end);
+    }
+
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Spot ", "Susan", "White", "Medium");
+        dog.setBoardStart(9,20,2018);
+        dog.setBoardEnd(11,21,2018);
+        System.out.println(dog);
+        System.out.println(dog.boarding(8,21,2018) + "\n");
+
+        Cat cat = new Cat("Tom", "Bob", "Black", "short");
+        cat.setBoardStart(9,20,2018);
+        cat.setBoardEnd(11,21,2018);
+        System.out.println(cat);
+        System.out.println(cat.boarding(11,21,2018));
+    }
+}
---
+++ Assignment6/src/Pet.java
@@ -0,0 +1,51 @@
+public class Pet {
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+    public static final int SPAYED = 3;
+    public static final int NEUTERED = 4;
+
+    private String petName;
+    private String ownerName;
+    private String color;
+    protected int sex;
+
+    public Pet(String petName, String ownerName, String color) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.color = color;
+    }
+
+    public String getPetName(){
+        return this.petName;
+    }
+
+    public String getOwnerName(){
+        return this.ownerName;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setSex(int sexid){
+        this.sex = sexid;
+    }
+
+    public String getSex(){
+        switch (this.sex){
+            case MALE:return "MALE";
+            case FEMALE:return "FEMALE";
+            case SPAYED:return "SPAYED";
+            case NEUTERED:return "NEUTERED";
+        }
+        return "";
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getPetName() +" owned by " + this.getOwnerName() +"\n"
+                + "Color:" + this.color + "\n" + "Sex:" +this.getSex());
+        return sb.toString();
+    }
+
+}
